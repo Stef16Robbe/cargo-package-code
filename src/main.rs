d4dead33dc07c4c7de4745ecb65b3cc95266402e
Stef <stef.robbe@gmail.com>
2021-12-23 23:09:10 +0100

    cleanup, added query size

--- src/main.rs
+++ src/main.rs
@@ -7,20 +7,13 @@ use reqwest::{Url, StatusCode};
 use reqwest::header::HeaderMap;
 
 // https://www.reddit.com/r/rust/comments/7hasv6/comment/dqpht6v/?utm_source=share&utm_medium=web2x&context=3
-#[derive(Deserialize, Debug)] struct Repository { html_url: String }
+#[derive(Deserialize, Debug)] struct Repository { html_url: String, description: String }
 #[derive(Deserialize, Debug)] struct Items { repository: Repository }
 #[derive(Deserialize, Debug)] struct Repo { items: Vec<Items> }
 
 impl Repo {
     // https://dev.to/hb/how-to-fetch-a-web-api-with-rust-1390
-    async fn get(package: &String, api_key: &String, github_name: &String) -> Result<Self, ExitFailure> {
-        // https://stackoverflow.com/a/63629726/10503012
-        // https://docs.github.com/en/search-github/searching-on-github/searching-code
-        let url = format!(
-            "https://api.github.com/search/code?q={package} +in:file +extension:toml",
-            package = package
-        );
-
+    async fn get(url: &String, api_key: &String, github_name: &String) -> Result<Self, ExitFailure> {
         let url = Url::parse(&*url)?;
         let mut headers = HeaderMap::new();
         headers.insert("User-Agent", github_name.parse().unwrap());
@@ -33,7 +26,7 @@ impl Repo {
             .send()
             .await?;
         
-        let res = match res.status() {
+        let res: Repo = match res.status() {
             StatusCode::OK => res.json().await?,
             _ => panic!("Failed to request the Github API, looks like you've been rate limited. Status code: {:?}", res.status())
         };
@@ -41,21 +34,25 @@ impl Repo {
         Ok(res)
     }
 
-    fn print_table(&self) -> Result<String, ExitFailure> {
-        let header = "─".repeat(24);
+    fn get_table(&self) -> Result<String, ExitFailure> {
+        // TODO:
+        // find a better way to determine table sizes...
+        let header = "─".repeat(120);
         let mut table: String = format!(
-            "{0: <1} │ {1: <20} \n{2}\n",
+            "{0: <1} │ {1: <50} │ {2: <100}\n{3}\n",
             " ",
             "Name",
+            "Description",
             header
         );
 
         let mut i = 0;
         for repo in &self.items {
             table += &format!(
-                "{0: <1} │ {1: <20}\n",
+                "{0: <1} │ {1: <50} │ {2: <100}\n",
                 i,
-                repo.repository.html_url
+                repo.repository.html_url,
+                repo.repository.description
             );
             i += 1;
         }
@@ -64,10 +61,25 @@ impl Repo {
     }
 }
 
+fn create_query(package: &String, repo_count: &u32) -> String {
+    // https://stackoverflow.com/a/63629726/10503012
+    // https://docs.github.com/en/search-github/searching-on-github/searching-code
+    let url = format!(
+        "https://api.github.com/search/code?per_page={repo_count}&q={package} +in:file +filename:Cargo +extension:toml +sort:updated-asc",
+        repo_count = repo_count,
+        package = package
+    );
+
+    println!("{}", url);
+
+    url
+}
+
 #[tokio::main]
 async fn main() -> Result<(), ExitFailure> {
     dotenv().ok();
 
+    // https://stackoverflow.com/a/60458834/10503012
     let matches = App::new("Cargo Package Code")
                                     .version("0.1")
                                     .author("Stef16Robbe <stef.robbe@gmail.com>")
@@ -79,16 +91,31 @@ async fn main() -> Result<(), ExitFailure> {
                                         .help("Set the Cargo package name to search for")
                                         .required(true)
                                         .takes_value(true))
+                                    .arg(Arg::with_name("count")
+                                        .short("c")
+                                        .long("count")
+                                        .value_name("COUNT")
+                                        .help("Set the amount of repositories returned")
+                                        .required(false)
+                                        .default_value("10")
+                                        .takes_value(true))
                                     .get_matches();
 
     // fltk-table
     let cargo_package = matches.value_of("name").unwrap().to_string();
+    let repo_count: u32 = match matches.value_of("count") {
+        Some(value) => value.parse().unwrap(),
+        None => 10u32,
+    };
+
     let api_key = env::var("GITHUB_API_KEY").unwrap();
     let username = env::var("GITHUB_USERNAME").unwrap();
 
-    println!("\nSearching for Repo's that use the {} in their toml file...", cargo_package);
-    let res = Repo::get(&cargo_package, &api_key, &username).await?;
-    println!("\n{}", Repo::print_table(&res)?);
+    let url = create_query(&cargo_package, &repo_count);
+
+    println!("\nSearching for Repo's that use the {} crate in their toml file...", cargo_package);
+    let res = Repo::get(&url, &api_key, &username).await?;
+    println!("\n{}", Repo::get_table(&res)?);
 
     Ok(())
 }
